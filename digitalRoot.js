ad5aa8a50ebe9da7d483ff2f709dbe5fc491513a
Against Lightning <69194169+AgainstLightning@users.noreply.github.com>
2021-10-24 02:50:45 -0400

    add Digital Root kata solution

---
+++ digitalRoot.js
@@ -0,0 +1,30 @@
+/*
+
+https://www.codewars.com/kata/541c8630095125aba6000c00
+
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, 
+continue reducing in this way until a single-digit number is produced. The input 
+will be a non-negative integer.
+
+Example:
+    16  -->  1 + 6 = 7
+    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+    132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+    493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+*/
+
+function digital_root(n) {
+    let root = n;
+    
+    while(root > 9) {
+      root = root
+              .toString()
+              .split("")
+              .reduce((s, n) => s + +n, 0)
+    }
+    
+    return root;
+}
